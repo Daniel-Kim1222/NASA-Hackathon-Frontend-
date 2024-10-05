fcddf7c36ef66b1cef4967ea5ca3ceff1c27f4a5
Olivia Huh <olihuh09@gmail.com>
2024-10-05 12:02:44 +0900

    created mainpage

---
+++ hwo-frontend/src/Pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainPage() {
+  {
+    return (
+      <div>
+        <h1>Home Page</h1> <p>Welcome to the main page of team GRIT FORCE!</p>{" "}
+      </div>
+    );
+  }
+}
+
+export default MainPage;
