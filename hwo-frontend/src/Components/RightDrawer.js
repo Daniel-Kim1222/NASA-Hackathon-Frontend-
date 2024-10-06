e2b33cc70257fd09056284e153cd69091ac3e5ee
Olivia Huh <ohuh2028@chadwickschool.org>
2024-10-06 16:28:27 +0900

    Updated filters

--- hwo-frontend/src/Components/RightDrawer.js
+++ hwo-frontend/src/Components/RightDrawer.js
@@ -14,16 +14,33 @@ import {
   Text,
   Tooltip,
   HStack,
+  Select,
+  Flex,
 } from "@chakra-ui/react";
 
 function RightDrawer({ applyFilters }) {
   const [isOpen, setIsOpen] = useState(false);
   // State for each filter
   const [maxDistance, setMaxDistance] = useState(null);
-  const [wavelength, setWavelength] = useState(null);
+  const [wavelength, setWavelength] = useState("");
   const [esi, setEsi] = useState(null);
   const [discoveryMethod, setDiscoveryMethod] = useState(null);
   const [telescopeDiameter, setTelescopeDiameter] = useState(null);
+  const [shouldReset, setShouldReset] = useState(false);
+
+  useEffect(() => {
+    if (shouldReset) {
+      handleApplyFilters(); // Only run after the states are updated
+      setShouldReset(false); // Reset the flag
+    }
+  }, [maxDistance, telescopeDiameter, wavelength, shouldReset]);
+
+  const handleReset = () => {
+    setMaxDistance(null);
+    setTelescopeDiameter(null);
+    setWavelength("");
+    setShouldReset(true); // Set the flag to true to trigger the effect
+  };
 
   // Toggle Drawer
   const toggleDrawer = () => {
@@ -37,7 +54,7 @@ function RightDrawer({ applyFilters }) {
     if (maxDistance !== null) payload.max_distance = maxDistance;
     if (telescopeDiameter !== null)
       payload.telescope_diameter = telescopeDiameter;
-    if (wavelength !== null) payload.wavelength = wavelength;
+    if (wavelength !== "") payload.wavelength = parseFloat(wavelength);
     if (esi !== null) payload.esi_threshold = esi;
     if (discoveryMethod !== null) payload.discovery_method = discoveryMethod;
 
@@ -51,6 +68,7 @@ function RightDrawer({ applyFilters }) {
     ) {
       payload.max_distance = 8600;
     }
+    console.log(JSON.stringify(payload));
 
     try {
       const response = await fetch(
@@ -147,28 +165,61 @@ function RightDrawer({ applyFilters }) {
                     </Tooltip>
                   </Slider>
                 </Box>
-                <HStack w="100%">
-                  <Button
-                    color={"brand.700"}
-                    backgroundColor={"brand.500"}
-                    onClick={handleApplyFilters}
-                    fontSize={"md"}
-                  >
-                    Apply
-                  </Button>
-                  <Button
-                    fontSize={"md"}
-                    color={"brand.700"}
-                    backgroundColor={"brand.500"}
-                    onClick={() => {
-                      setMaxDistance(null);
-                      handleApplyFilters();
-                    }}
-                  >
-                    Reset
-                  </Button>
-                </HStack>
                 {/* Telescope & Diameter Slider */}
+                <Box>
+                  <Text align="center" fontSize="sm" color="brand.400">
+                    Telescope Diameter(m) & Wavelength(µm)
+                  </Text>
+                  <Slider
+                    defaultValue={0.1}
+                    min={0}
+                    max={24.5}
+                    step={0.1}
+                    onChange={(value) => setTelescopeDiameter(value)}
+                    my={2}
+                  >
+                    <SliderTrack bg="brand.700" height="8px">
+                      <SliderFilledTrack bg="brand.600" />
+                    </SliderTrack>
+                    <Tooltip
+                      label={`Telescope Diameter: ${telescopeDiameter}`}
+                      hasArrow
+                    >
+                      <SliderThumb boxSize={4} bg="brand.500" />
+                    </Tooltip>
+                  </Slider>
+                  <Select
+                    placeholder="Select Wavelength"
+                    fontSize="md"
+                    value={wavelength}
+                    onChange={(e) => setWavelength(e.target.value)}
+                    bg="brand.600"
+                    color="white"
+                    borderColor="brand.700"
+                    mb={2}
+                    padding={0}
+                    h="30px"
+                    focusBorderColor="brand.500"
+                  >
+                    <option value="0.1">Far UV: 0.1 – 0.2 µm</option>
+                    <option value="0.2">Near UV: 0.2 – 0.4 µm</option>
+                    <option value="0.3">U-band: 0.3 – 0.4 µm</option>
+                    <option value="0.4">B-band: 0.4 – 0.5 µm</option>
+                    <option value="0.5">V-band: 0.5 – 0.6 µm</option>
+                    <option value="0.6">R-band: 0.6 – 0.7 µm</option>
+                    <option value="0.7">I-band: 0.7 – 0.8 µm</option>
+                    <option value="1.0">Y-band: 1.0 – 1.1 µm</option>
+                    <option value="1.1">J-band: 1.1 – 1.4 µm</option>
+                    <option value="1.5">H-band: 1.5 – 1.8 µm</option>
+                    <option value="2.0">K-band: 2.0 – 2.4 µm</option>
+                    <option value="3.0">L-band: 3.0 – 4.0 µm</option>
+                    <option value="4.5">M-band: 4.5 – 5.0 µm</option>
+                    <option value="8.0">N-band: 8.0 – 13.0 µm</option>
+                    <option value="16.0">Q-band: 16.0 – 20.0 µm</option>
+                    <option value="30">S-band: 30 – 100 µm</option>
+                    <option value="100">T-band: 100 – 300 µm</option>
+                  </Select>
+                </Box>
                 <HStack w="100%">
                   <Button
                     color={"brand.700"}
@@ -182,10 +233,7 @@ function RightDrawer({ applyFilters }) {
                     fontSize={"md"}
                     color={"brand.700"}
                     backgroundColor={"brand.500"}
-                    onClick={() => {
-                      setMaxDistance(null);
-                      handleApplyFilters();
-                    }}
+                    onClick={handleReset}
                   >
                     Reset
                   </Button>
